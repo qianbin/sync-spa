dfdc43e7da2b99d9d2fb83d969365477de14957d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-31 17:17:59 +0000

    Deploy qianbin/sync-spa to github.com/qianbin/sync-spa.git:gh-pages

---
+++ precache-manifest.abbb7074e77d4ba72448b797e3099386.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a169d0ce625909423d2e58fe46aee9",
+    "url": "2722a1f5d7ca17adce676581a899ff55.js"
+  },
+  {
+    "revision": "cbacb2cad799ef2b416ecab65c12e64d",
+    "url": "ad08d5ff213ebc083bb4.worker.js"
+  },
+  {
+    "revision": "dc18eabb27a06c54374a",
+    "url": "css/app.10c2f348.css"
+  },
+  {
+    "revision": "39083cdef834fe0d7ee39e0272d1aa76",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.6d14df8c.woff"
+  },
+  {
+    "revision": "d960b795b343e35150c1a0aa3c38c16b",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.c8685531.woff2"
+  },
+  {
+    "revision": "70fe4a66ffafedfdc0f4ce530292b594",
+    "url": "index.html"
+  },
+  {
+    "revision": "dc18eabb27a06c54374a",
+    "url": "js/app.1d68fdaf.js"
+  },
+  {
+    "revision": "8b69958084c467dc3bce",
+    "url": "js/vendor.8b699580.js"
+  },
+  {
+    "revision": "e985202df774890177e9f93a7f245753",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "344203a9121354d257554ebe926816c6",
+    "url": "statics/app-logo-128x128.png"
+  },
+  {
+    "revision": "30109ef130e349e559a2759a3412c863",
+    "url": "statics/icons/apple-icon-120x120.png"
+  },
+  {
+    "revision": "12eb78fc327bc6caea208bbe7ee66f90",
+    "url": "statics/icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "b1218b2796ed7cc961de752166f070ef",
+    "url": "statics/icons/apple-icon-167x167.png"
+  },
+  {
+    "revision": "dd8b8543b777bffcf513caea3a7bc18d",
+    "url": "statics/icons/apple-icon-180x180.png"
+  },
+  {
+    "revision": "2a2a862d0717b468f0cc8d3c175bcfc7",
+    "url": "statics/icons/apple-launch-1125x2436.png"
+  },
+  {
+    "revision": "0e94c29c0d64f50d0db4e109c934a274",
+    "url": "statics/icons/apple-launch-1242x2208.png"
+  },
+  {
+    "revision": "c75702db5b17d695ae4a475e18fb05ab",
+    "url": "statics/icons/apple-launch-1242x2688.png"
+  },
+  {
+    "revision": "40027ecbc4fdd2ae419edc18fd249068",
+    "url": "statics/icons/apple-launch-1536x2048.png"
+  },
+  {
+    "revision": "a024b35a9ff7f1b8810f9a7484c767ef",
+    "url": "statics/icons/apple-launch-1668x2224.png"
+  },
+  {
+    "revision": "d9f415ae81dd94fc89a97201a9756890",
+    "url": "statics/icons/apple-launch-1668x2388.png"
+  },
+  {
+    "revision": "c14aebcced6a1d29b64d23b857095258",
+    "url": "statics/icons/apple-launch-2048x2732.png"
+  },
+  {
+    "revision": "0deb02027e35aaf593b10b7d1c8e5656",
+    "url": "statics/icons/apple-launch-640x1136.png"
+  },
+  {
+    "revision": "dde0a355e95d766a9cbfa91aa946227b",
+    "url": "statics/icons/apple-launch-750x1334.png"
+  },
+  {
+    "revision": "8b0e213a3474b4526285ffd1e57cc9c7",
+    "url": "statics/icons/apple-launch-828x1792.png"
+  },
+  {
+    "revision": "344203a9121354d257554ebe926816c6",
+    "url": "statics/icons/favicon-128x128.png"
+  },
+  {
+    "revision": "8970e2994c040ae79a4d18059bbde887",
+    "url": "statics/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "a8a1ff91e54b8c267eed0e9034166323",
+    "url": "statics/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "f5b845de64cbbbaa06c5cd8bd615b02b",
+    "url": "statics/icons/favicon-96x96.png"
+  },
+  {
+    "revision": "3b44d926088edfbb95185c989db12a9d",
+    "url": "statics/icons/favicon.ico"
+  },
+  {
+    "revision": "344203a9121354d257554ebe926816c6",
+    "url": "statics/icons/icon-128x128.png"
+  },
+  {
+    "revision": "a8306595aca6f01e49abf04575711cf8",
+    "url": "statics/icons/icon-192x192.png"
+  },
+  {
+    "revision": "b94c50d10eaef8eaef791d70e91c5552",
+    "url": "statics/icons/icon-256x256.png"
+  },
+  {
+    "revision": "20bf7d98fcaf9eadf86bfef328fd9c4f",
+    "url": "statics/icons/icon-384x384.png"
+  },
+  {
+    "revision": "18d29d1d2ca4276a2ed98bc3798fad8d",
+    "url": "statics/icons/icon-512x512.png"
+  },
+  {
+    "revision": "ccc933b08d0d19e18b5b3f648a3b11eb",
+    "url": "statics/icons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "48348d04452910e8f6e05c1c97e9f3ce",
+    "url": "statics/icons/safari-pinned-tab.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,36 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "precache-manifest.abbb7074e77d4ba72448b797e3099386.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "org.vechain.sync2"});
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("index.html"));
+
+workbox.routing.registerRoute(/^https:\/\/vechain.github.io\/token-registry\/assets\//, new workbox.strategies.CacheFirst({ "cacheName":"token-icons", plugins: [new workbox.cacheableResponse.Plugin({ statuses: [ 0, 200 ] })] }), 'GET');
